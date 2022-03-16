6d84081645bf39f1d70daa7ce440b6a12dc8ab0d
sntsabode <snts.abode@gmail.com>
2022-03-16 18:03:32 +0200

    feat: main

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"gy-serv-init/src/core"
+	"os"
+)
+
+func main() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(os.Args)
+
+	if len(os.Args) > 2 {
+		panic("Too many arguments")
+	} else if len(os.Args) < 2 {
+		panic("Too few arguments")
+	}
+
+	fmt.Printf("Creating a project named %s\n", os.Args[1])
+	core.Main(&pwd, &os.Args[1])
+}
